e2ec5dcff0d48ef42c8b4b236e062d4f77e3f5a2
Calico <112098778+HackerCalico@users.noreply.github.com>
2024-07-31 13:44:15 +0800

    Delete Shell directory

--- Shell/HttpReverseShell/AntiSandbox.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// 抗沙箱: 获取 D 盘文件目录
-void AntiSandbox(char**, int*);
--- Shell/HttpReverseShell/CommandController.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void ConstructPendingSessionInfo(char*, char*, char*, int*);
-void ConstructSessionInfo(char*, char*, char*, int*);
